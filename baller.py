8c99131574e552ffe3ffeaa7c4366a64139049d6
sigurd <sigurdskudal@gmail.com>
2026-01-13 13:38:10 +0100

    Implement Baller class updates and add oppdaterAlleBaller function

--- baller.py
+++ baller.py
@@ -1,5 +1,7 @@
+from konstanter import *
 from random import randint
 import numpy as np
+
 class Baller:
     def __init__(self,x,y):
         self.størrelse=10
@@ -10,9 +12,29 @@ class Baller:
         self.vx=np.sin(self.vinkel)*5
         self.vy=-np.cos(self.vinkel)*5
     
+    def kill(self,baller:list):
+        baller.remove(self)
+        
+
     def oppdater(self):
         self.x+=self.vx
         self.y+=self.vy
+        if self.x-self.størrelse<=0 or self.x+self.størrelse>=VINDU_BREDDE:
+            self.vx*=-1
+        if self.y-self.størrelse<=0:
+            self.vy*=-1
+        if self.y-self.størrelse>VINDU_HOYDE:
+            self.kill()
+    
+
+
+
+def spawnBall(baller:list):
+    pass
+
+def oppdaterAlleBaller(baller:list):
+    for b in baller:
+        b.oppdater()
 
 
 

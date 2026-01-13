ddf7196c87d32c10a1e7b01609edd425bc3ff9f2
Woffsy <wilhelm.hungnes@gmail.com>
2026-01-13 13:25:10 +0100

    Added a working paddle to main

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ clock = pg.time.Clock()
 def main():
     running = True
     
+    paddle = Paddle()
+    
+    
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -19,8 +22,15 @@ def main():
             elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                 running = False
 
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            paddle.x -= 5
+        if keys[pg.K_RIGHT]:
+            paddle.x += 5
+
         vindu.fill(WHITE)
         
+        paddle.tegnSelv(vindu)
         
         pg.display.flip()
         clock.tick(FPS)
--- paddle.py
+++ paddle.py
@@ -6,7 +6,7 @@ class Paddle():
         self.x = VINDU_BREDDE/2
         self.y = VINDU_HOYDE-50
         
-        self.color = WHITE
+        self.color = BLACK
         
     def tegnSelv(self, vindu):
         pg.draw.rect(vindu, self.color, (self.x, self.y, 50, 20))

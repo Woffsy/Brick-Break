a5a56e5d512f203f65508d685bf13c1497a71506
Woffsy <wilhelm.hungnes@gmail.com>
2026-01-14 08:34:00 +0100

    Refactor Baller class to use dynamic velocity based on fart and improve collision response with paddle

--- baller.py
+++ baller.py
@@ -10,8 +10,8 @@ class Baller:
         self.y=y
         self.fart=5
         self.vinkel=randint(-80,80)/180*np.pi
-        self.vx=np.sin(self.vinkel)*5
-        self.vy=-np.cos(self.vinkel)*5
+        self.vx=np.sin(self.vinkel)*self.fart
+        self.vy=-np.cos(self.vinkel)*self.fart
         self.baller=baller
         self.vindu=vindu
         baller.append(self)
@@ -37,8 +37,12 @@ class Baller:
         if pg.Rect(self.x - self.størrelse, self.y - self.størrelse, self.størrelse * 2, self.størrelse * 2).colliderect(pg.Rect(paddle.x, paddle.y, paddle.bredde, paddle.hoyde)):
             self.vy*=-1
             self.y = paddle.y - self.størrelse - 2
-            print("kollisjon")
+            
+            nyvinkel = (paddle.x+paddle.bredde/2-self.x)/180*-np.pi
+            self.vx = np.sin(nyvinkel)*self.fart
+            self.vy = -np.cos(nyvinkel)*self.fart
 
+            print(f"lengde fra sentrum {(paddle.x+paddle.bredde/2-self.x)*-1}")
         
         
 def spawnBall(baller:list):

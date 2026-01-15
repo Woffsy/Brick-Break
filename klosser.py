c28b2a96c4257abdedb8561dc7dbb2af76b25256
Woffsy <wilhelm.hungnes@gmail.com>
2026-01-15 10:04:24 +0100

    Remove type ignore comment for spawnPowerUp function in oppdaterKloss

--- klosser.py
+++ klosser.py
@@ -77,7 +77,7 @@ def oppdaterKloss(klosser, baller: list, vindu):
         else:
             klosser.remove(kloss)
             if randint(0, 100) > 90:
-                spawnPowerUp(kloss, powerUps) # type: ignore
+                spawnPowerUp(kloss, powerUps)
             
     if len(klosser) == 0:
         lagKlosser(klosser, vindu)
